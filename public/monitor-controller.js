58f4629b4ab1ad743104d865e2cff36031d8ffc6
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2017-02-06 11:46:58 +0100

    remove trailing slash

--- public/monitor-controller.js
+++ public/monitor-controller.js
@@ -28,7 +28,7 @@ function MonitorController($scope, $interval, $http) {
 
     $scope.callService = function() {
             var startTime = new Date().getTime();
-	    $http.get(document.location.href + '/status').
+	    $http.get(document.location.href.replace(/\/+$/, "") + '/status');
 		success(function(response) {
 		    var responseTime = new Date().getTime() - startTime;
 		    var key = response.key;
